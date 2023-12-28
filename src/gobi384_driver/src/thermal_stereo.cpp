ec62586b63f2c3b99b38094ee5a03195796507a9
raymond <raymond.lau.lyf@gmail.com>
2023-12-29 02:30:12 +0800

    milestone: temporary launch the sthereo function Ok

--- src/gobi384_driver/src/thermal_stereo.cpp
+++ src/gobi384_driver/src/thermal_stereo.cpp
@@ -35,38 +35,13 @@ using namespace std;
 using namespace cv;
 using namespace std::chrono;
 
-system_clock::time_point now;
-time_t tt;
-tm utc_tm;
-tm local_tm;
-tm last_tm;
-int timestamp_modifier_i;
-char timestamp_modifier_c;
-cv::Mat thermal_img, thermal_img_inv, color_img;
+
 // image_transport::Publisher image_pub;
 ros::Publisher pub_image1, pub_image_clahe1, pub_image16_1;
 ros::Publisher pub_image2, pub_image_clahe2, pub_image16_2;
 
-/*
-//NBL: ROS Compliance
-std_msgs::String record;*/
-string recordData = "1";
-bool camerasInitialized;
 
-unsigned int imageCnt;
-/*
-//NBL: ROS Compliance
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-    record = *msg;
 
-    //ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
-*/
-/*
-int AcquireImage()
-{
-}*/
 string type2str(int type)
 {
     string r;
@@ -116,6 +91,7 @@ void cameraThreadFirst(ros::NodeHandle nh)
         ErrCode errorCode = 0; // Used to store returned errorCodes from the SDK functions.
         word *frameBuffer = 0; // 16-bit buffer to store the capture frame.
         dword frameSize = 0;   // The size in bytes of the raw image.
+        cv::Mat thermal_img, thermal_img_inv, color_img;
 
         // Open a connection to the first detected camera by using connection string cam://0
         printf("[cam 1] Opening connection to cam 1 \n");
@@ -254,7 +230,149 @@ void cameraThreadFirst(ros::NodeHandle nh)
 }
 void cameraThreadSecond(ros::NodeHandle nh)
 {
-    
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    try{
+        // Variables
+        XCHANDLE handle = 0;   // Handle to the camera
+        ErrCode errorCode = 0; // Used to store returned errorCodes from the SDK functions.
+        word *frameBuffer = 0; // 16-bit buffer to store the capture frame.
+        dword frameSize = 0;   // The size in bytes of the raw image.
+        cv::Mat thermal_img, thermal_img_inv, color_img;
+
+        // Open a connection to the first detected camera by using connection string cam://0
+        printf("[cam 2] Opening connection to cam 2 \n");
+        handle = XC_OpenCamera("cam://1");
+
+
+        if (!XC_IsInitialised(handle)) { 
+            printf("[cam 2]Could not open a connection to camera.\n");
+            throw std::runtime_error("[cam 2]Could not open a connection to camera.");
+        }
+
+        std::string config_file;
+        nh.getParam("config_file", config_file);
+        const char *settings = config_file.c_str();
+        
+        if (XC_IsInitialised(handle))
+        {
+            // ... start capturing
+
+            // // Save settings
+            // printf("Saving settings.\n");
+            // XC_SaveSettings(handle, settings);
+
+            // XC_LoadSettings(handle, settings);
+            // printf("[cam 2] Load Settings\n");
+
+            printf("[cam 2] Start capturing.\n");
+            if ((errorCode = XC_StartCapture(handle)) != I_OK)
+            {
+                printf("[cam 2] Could not start capturing, errorCode: %lu\n", errorCode);
+            }
+            else if (XC_IsCapturing(handle)) // When the camera is capturing ...
+            {
+                while (ros::ok())
+                {
+                    // Determine native framesize.
+                    frameSize = XC_GetFrameSize(handle);
+
+                    // Initialize the 16-bit buffer.
+                    frameBuffer = new word[frameSize / 2];
+                    // ... grab a frame from the camera.
+                    // printf("Grabbing a frame.\n");
+                    ROS_INFO("[cam 2] Grabbing a frame.\r");
+                    if ((errorCode = XC_GetFrame(handle, FT_NATIVE, XGF_Blocking, frameBuffer, frameSize)) != I_OK)
+                    {
+                        printf("[cam 2] Problem while fetching frame, errorCode %lu", errorCode);
+                    }
+                    else
+                    {
+                        int h = XC_GetHeight(handle);
+                        int w = XC_GetWidth(handle);
+                        thermal_img =
+                            cv::Mat(h, w, CV_16UC1, frameBuffer); /*convert to OpenCV*/
+                        Mat img8;
+                        string ty = type2str(thermal_img.type());
+                        // printf("Matrix: %s %dx%d \n", ty.c_str(), thermal_img.cols, thermal_img.rows);
+                        thermal_img.convertTo(img8, CV_8UC1, 1 / 256.0); // convert image to 8bit
+                        // Mat img8;
+                        // normalize(thermal_img, img8, 0, 255, NORM_MINMAX);
+                        // convertScaleAbs(img8, img8);
+                        // medianBlur(img8,img8,3);
+
+
+
+                        // // clahe
+                        // cv::Mat img8_clahe;
+                        // cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(1.0, cv::Size(32, 32));
+                        // clahe->apply(img8, img8_clahe);
+
+
+                        // cv::equalizeHist(img8,img8);
+                        //    imshow("1",thermal_img);
+                        //  waitKey(1);
+
+                        ros::Time t = ros::Time::now();
+                        
+                        sensor_msgs::ImagePtr output_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", img8).toImageMsg();
+                        output_msg->header.stamp = t;
+
+                        // sensor_msgs::ImagePtr output_msg_clahe = cv_bridge::CvImage(std_msgs::Header(), "mono8", img8_clahe).toImageMsg();
+                        // output_msg_clahe->header.stamp = t;
+
+                        sensor_msgs::ImagePtr msg_thermal = cv_bridge::CvImage(std_msgs::Header(), "mono16", thermal_img).toImageMsg();
+                        msg_thermal->header.stamp = t;
+
+                        static long long iter_num;
+                        if (iter_num % 4 == 0)
+                        {
+                            // pub_image_clahe1.publish(output_msg_clahe);
+                            pub_image2.publish(output_msg);
+                            pub_image16_2.publish(msg_thermal);
+                        }
+                        iter_num++;
+
+                        if (frameBuffer != 0)
+                        {
+                            delete[] frameBuffer;
+                            frameBuffer = 0;
+                        }
+                    }
+                }
+                if (XC_IsCapturing(handle))
+                {
+                    // ... stop capturing.
+                    printf("[cam 2] Stop capturing.\n");
+                    if ((errorCode = XC_StopCapture(handle)) != I_OK)
+                    {
+                        printf("[cam 2] Could not stop capturing, errorCode: %lu\n", errorCode);
+                    }
+                }
+
+                // When the handle to the camera is still initialised ...
+                if (XC_IsInitialised(handle))
+                {
+                    printf("[cam 2] Closing connection to camera.\n");
+                    XC_CloseCamera(handle);
+                }
+
+                printf("[cam 2] Clearing buffers.\n");
+                if (frameBuffer != 0)
+                {
+                    delete[] frameBuffer;
+                    frameBuffer = 0;
+                }
+            }
+            else
+            {
+                printf("[cam 2] Initialization failed\n");
+            }
+        }
+
+
+    } catch (const std::exception& e) {
+        std::cerr << "[cam 2] Error: " << e.what() << std::endl;
+    } 
 }
 
 
